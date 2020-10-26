62a880ec50a58815db6154eb668e80273bbe7e8a
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2020-10-26 15:11:50 +0000

    Reduced complexity by adding early return
    Resolves: BI-4756

--- src/main/java/uk/gov/companieshouse/efs/web/security/CompanyAuthFilter.java
+++ src/main/java/uk/gov/companieshouse/efs/web/security/CompanyAuthFilter.java
@@ -1,17 +1,5 @@
 package uk.gov.companieshouse.efs.web.security;
 
-import static uk.gov.companieshouse.efs.web.categorytemplates.controller.CategoryTypeConstants.INSOLVENCY;
-
-import java.io.IOException;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import uk.gov.companieshouse.api.model.ApiResponse;
 import uk.gov.companieshouse.api.model.efs.formtemplates.FormTemplateApi;
 import uk.gov.companieshouse.api.model.efs.submissions.SubmissionApi;
@@ -26,6 +14,19 @@ import uk.gov.companieshouse.session.Session;
 import uk.gov.companieshouse.session.handler.SessionHandler;
 import uk.gov.companieshouse.session.model.SignInInfo;
 import uk.gov.companieshouse.session.model.UserProfile;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static uk.gov.companieshouse.efs.web.categorytemplates.controller.CategoryTypeConstants.INSOLVENCY;
 
 public class CompanyAuthFilter extends AuthFilter {
 
@@ -42,15 +43,15 @@ public class CompanyAuthFilter extends AuthFilter {
     /**
      * Constructor.
      *
-     * @param environmentReader         dependency
-     * @param apiClientService          dependency
-     * @param formTemplateService       dependency
-     * @param categoryTemplateService   dependency
+     * @param environmentReader       dependency
+     * @param apiClientService        dependency
+     * @param formTemplateService     dependency
+     * @param categoryTemplateService dependency
      */
     public CompanyAuthFilter(final EnvironmentReader environmentReader,
-        final ApiClientService apiClientService,
-        final FormTemplateService formTemplateService,
-        CategoryTemplateService categoryTemplateService) {
+                             final ApiClientService apiClientService,
+                             final FormTemplateService formTemplateService,
+                             CategoryTemplateService categoryTemplateService) {
         super(environmentReader);
         this.apiClientService = apiClientService;
         this.formTemplateService = formTemplateService;
@@ -59,57 +60,60 @@ public class CompanyAuthFilter extends AuthFilter {
 
     @Override
     public void doFilter(
-        ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
         Matcher matcher = EFS_SUBMISSION_WITH_COMPANY.matcher(httpServletRequest.getRequestURI());
 
-        if ("GET".equalsIgnoreCase(httpServletRequest.getMethod()) && matcher.find()) {
+        if (!("GET".equalsIgnoreCase(httpServletRequest.getMethod()) && matcher.find())) {
+            chain.doFilter(request, response);
+            return;
+        }
 
-            String efsSubmissionId = matcher.group(1);
+        String efsSubmissionId = matcher.group(1);
 
-            ApiResponse<SubmissionApi> submissionApiResponse = apiClientService.getSubmission(efsSubmissionId);
+        ApiResponse<SubmissionApi> submissionApiResponse = apiClientService.getSubmission(efsSubmissionId);
 
-            SubmissionApi submissionApi = submissionApiResponse.getData();
+        SubmissionApi submissionApi = submissionApiResponse.getData();
 
-            SubmissionFormApi submissionFormApi = submissionApi.getSubmissionForm();
+        SubmissionFormApi submissionFormApi = submissionApi.getSubmissionForm();
 
-            // The form could be null if the user hasn't progressed far enough in the journey to select a form type
-            if (submissionFormApi != null) {
+        // The form could be null if the user hasn't progressed far enough in the journey to select a form type
+        if (submissionFormApi != null) {
 
-                String formType = submissionFormApi.getFormType();
+            String formType = submissionFormApi.getFormType();
 
-                ApiResponse<FormTemplateApi> formTemplateResponse = formTemplateService.getFormTemplate(formType);
+            ApiResponse<FormTemplateApi> formTemplateResponse = formTemplateService.getFormTemplate(formType);
 
-                String formCategory = formTemplateResponse.getData().getFormCategory();
+            String formCategory = formTemplateResponse.getData().getFormCategory();
 
-                boolean isAuthenticationRequired = formTemplateResponse.getData().isAuthenticationRequired();
+            boolean isAuthenticationRequired = formTemplateResponse.getData().isAuthenticationRequired();
 
-                if (isAuthenticationRequired) {
-                    //Get the sign in info from the CH session
-                    Session chSession = (Session) request.getAttribute(SessionHandler.CHS_SESSION_REQUEST_ATT_KEY);
-                    SignInInfo signInInfo = new SignInInfo();
-                    if (chSession != null) {
-                        signInInfo = chSession.getSignInInfo();
-                    }
+            if (isAuthenticationRequired) {
+                //Get the sign in info from the CH session
+                Session chSession = (Session) request.getAttribute(SessionHandler.CHS_SESSION_REQUEST_ATT_KEY);
+                SignInInfo signInInfo = new SignInInfo();
+                if (chSession != null) {
+                    signInInfo = chSession.getSignInInfo();
+                }
 
-                    String companyNumber = matcher.group(2);
+                String companyNumber = matcher.group(2);
 
                     /*
                         According to the sign in info, check:
                         If the company number is authorised
                         If the email address is on the allow list for insolvency forms
                      */
-                    if (!isAuthorisedForCompany(signInInfo, companyNumber)
+                if (!isAuthorisedForCompany(signInInfo, companyNumber)
                         && !isOnAllowList(signInInfo, formCategory)) {
 
-                        //Redirect for company authentication (scope specified)
-                        redirectForAuth(chSession, httpServletRequest, httpServletResponse, companyNumber, false);
-                    }
+                    //Redirect for company authentication (scope specified)
+                    redirectForAuth(chSession, httpServletRequest, httpServletResponse, companyNumber, false);
                 }
             }
+
         }
 
         chain.doFilter(request, response);
@@ -117,20 +121,22 @@ public class CompanyAuthFilter extends AuthFilter {
 
     /**
      * Determines whether a signed in user is authorised to act on behalf of a company.
-     * @param signInInfo User's signed in info retrieved from their session
+     *
+     * @param signInInfo    User's signed in info retrieved from their session
      * @param companyNumber The company to act on behalf of
      * @return true or false
      */
     private boolean isAuthorisedForCompany(SignInInfo signInInfo, String companyNumber) {
         String authorisedCompany = signInInfo.getCompanyNumber();
         return (authorisedCompany != null
-            && authorisedCompany.equalsIgnoreCase(companyNumber))
-            && hasAuthorisedCompanyScope(signInInfo, companyNumber);
+                && authorisedCompany.equalsIgnoreCase(companyNumber))
+                && hasAuthorisedCompanyScope(signInInfo, companyNumber);
     }
 
     /**
      * Determines whether a signed in user has authorised scope to act on behalf of a company.
-     * @param signInInfo User's signed in info retrieved from their session
+     *
+     * @param signInInfo    User's signed in info retrieved from their session
      * @param companyNumber The company to act on behalf of
      * @return true or false
      */
@@ -154,7 +160,8 @@ public class CompanyAuthFilter extends AuthFilter {
      * Determines whether a signed in users email address is within the company authentication allow list.
      * If the email address is within the allow list, and an insolvency form has been selected, then
      * the company does not require authentication
-     * @param signInInfo User's signed in info retrieved from their session
+     *
+     * @param signInInfo   User's signed in info retrieved from their session
      * @param formCategory The form category
      * @return true or false
      */
