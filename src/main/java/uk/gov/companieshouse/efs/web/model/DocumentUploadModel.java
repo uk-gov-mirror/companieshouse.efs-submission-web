722b12d83238efa2611f6f521aba6ba6da31b622
Phil Glover <pglover@companieshouse.gov.uk>
2021-01-08 15:36:23 +0000

    refactor getAllowedFileExtensions method, add unit test and fix sonar issue in DataCacheConfig

--- src/main/java/uk/gov/companieshouse/efs/web/model/DocumentUploadModel.java
+++ src/main/java/uk/gov/companieshouse/efs/web/model/DocumentUploadModel.java
@@ -3,6 +3,7 @@ package uk.gov.companieshouse.efs.web.model;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -146,10 +147,19 @@ public class DocumentUploadModel {
 
     public String getAllowedFileExtensions() {
         final List<String> extensions = config.getDistinctExtensions().stream().sorted().collect(Collectors.toList());
-        if (!extensions.isEmpty()) {
-            return String.join(", ", extensions);
-        }
-        return "";
+        return extensions.stream().collect(Collectors.collectingAndThen(Collectors.toList(),
+            joiningLastDelimiter(", ", " or ")));
+    }
+
+    public static Function<List<String>, String> joiningLastDelimiter(
+        String delimiter, String lastDelimiter) {
+        return list -> {
+            int last = list.size() - 1;
+            if (last < 1) return String.join(delimiter, list);
+            return String.join(lastDelimiter,
+                String.join(delimiter, list.subList(0, last)),
+                list.get(last));
+        };
     }
 
     @Override
---
+++ src/test/java/uk/gov/companieshouse/efs/web/model/DocumentUploadModelTest.java
@@ -0,0 +1,54 @@
+package uk.gov.companieshouse.efs.web.model;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import uk.gov.companieshouse.efs.web.configuration.FileUploadConfiguration;
+
+class DocumentUploadModelTest {
+
+    private DocumentUploadModel documentUploadModel;
+    private FileUploadConfiguration fileUploadConfiguration;
+    private FileUploadConfiguration.FileType fileType;
+
+    @Test
+    void getAllowedFileExtensionsWhenSingular() {
+        fileUploadConfiguration = new FileUploadConfiguration();
+        List<String> extensions = Collections.singletonList("PDF");
+        setConfig(extensions);
+
+        assertThat(documentUploadModel.getAllowedFileExtensions(), is("PDF"));
+    }
+
+    @Test
+    void getAllowedFileExtensionsWhenMultiple() {
+        fileUploadConfiguration = new FileUploadConfiguration();
+        List<String> extensions = Arrays.asList("PDF", "PDF2", "PDF3");
+        setConfig(extensions);
+
+        assertThat(documentUploadModel.getAllowedFileExtensions(), is("PDF, PDF2 or PDF3"));
+    }
+
+    @Test
+    void getAllowedFileExtensionsWhenEmpty() {
+        fileUploadConfiguration = new FileUploadConfiguration();
+        List<String> extensions = Collections.emptyList();
+        setConfig(extensions);
+
+        assertThat(documentUploadModel.getAllowedFileExtensions(), is(""));
+    }
+
+    private void setConfig(final List<String> extensions) {
+        FileUploadConfiguration.FileType fileType =
+            new FileUploadConfiguration.FileType("pdf", extensions);
+        List<FileUploadConfiguration.FileType> fileTypes = new ArrayList<>();
+        fileTypes.add(fileType);
+        fileUploadConfiguration.setAllowedTypes(fileTypes);
+        documentUploadModel = new DocumentUploadModel(fileUploadConfiguration);
+    }
+}
