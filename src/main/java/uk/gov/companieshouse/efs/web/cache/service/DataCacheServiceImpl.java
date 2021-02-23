1d2d3c961aa042064db15b7041ca4388c858fc4b
Simon Boyd <sboyd@companieshouse.gov.uk>
2021-02-23 15:21:31 +0000

    Add caching to FormTemplateServiceImpl

--- src/main/java/uk/gov/companieshouse/efs/web/cache/service/DataCacheServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/efs/web/cache/service/DataCacheServiceImpl.java
@@ -1,8 +1,11 @@
 package uk.gov.companieshouse.efs.web.cache.service;
 
 import static uk.gov.companieshouse.efs.web.configuration.DataCacheConfig.ALL_CATEGORIES;
+import static uk.gov.companieshouse.efs.web.configuration.DataCacheConfig.ALL_FORMS;
 import static uk.gov.companieshouse.efs.web.configuration.DataCacheConfig.CATEGORY_BY_ID;
 import static uk.gov.companieshouse.efs.web.configuration.DataCacheConfig.CATEGORY_BY_PARENT;
+import static uk.gov.companieshouse.efs.web.configuration.DataCacheConfig.FORM_BY_CATEGORY;
+import static uk.gov.companieshouse.efs.web.configuration.DataCacheConfig.FORM_BY_ID;
 import static uk.gov.companieshouse.efs.web.configuration.DataCacheConfig.IP_ALLOW_LIST;
 import static uk.gov.companieshouse.efs.web.configuration.DataCacheConfig.TOP_LEVEL_CATEGORY;
 
@@ -22,8 +25,10 @@ public class DataCacheServiceImpl implements DataCacheService {
     }
 
     @Scheduled(fixedDelayString = "${cache.eviction.delay.ms}")
-    @CacheEvict(cacheNames = {ALL_CATEGORIES, CATEGORY_BY_ID, CATEGORY_BY_PARENT, TOP_LEVEL_CATEGORY, IP_ALLOW_LIST},
-        allEntries = true)
+    @CacheEvict(
+            cacheNames = {ALL_CATEGORIES, CATEGORY_BY_ID, CATEGORY_BY_PARENT, TOP_LEVEL_CATEGORY,
+                    ALL_FORMS, FORM_BY_ID, FORM_BY_CATEGORY, IP_ALLOW_LIST},
+            allEntries = true)
     @Override
     public void clearAllCaches() {
         logger.debug(
--- src/main/java/uk/gov/companieshouse/efs/web/configuration/DataCacheConfig.java
+++ src/main/java/uk/gov/companieshouse/efs/web/configuration/DataCacheConfig.java
@@ -17,13 +17,20 @@ public class DataCacheConfig {
     public static final String CATEGORY_BY_ID = "category-by-id";
     public static final String CATEGORY_BY_PARENT = "category-by-parent";
     public static final String TOP_LEVEL_CATEGORY = "top-level-category";
+    public static final String ALL_FORMS = "all-forms";
+    public static final String FORM_BY_ID = "form-by-id";
+    public static final String FORM_BY_CATEGORY = "form-by-category";
     public static final String IP_ALLOW_LIST = "ip-allow-list";
     public static final String SUBMISSION_BY_ID = "submission-by-id";
+
+    public static final String[] REF_DATA_CACHE_NAMES =
+            {ALL_CATEGORIES, CATEGORY_BY_ID, CATEGORY_BY_PARENT, TOP_LEVEL_CATEGORY,
+                    ALL_FORMS, FORM_BY_ID, FORM_BY_CATEGORY, IP_ALLOW_LIST};
 
     // although this method is unused it prevents sonar considering this a utility class
     // with a public constructor
     @Bean
-    public List<String> cacheNames() {
-        return Arrays.asList(ALL_CATEGORIES, CATEGORY_BY_ID, CATEGORY_BY_PARENT, TOP_LEVEL_CATEGORY, IP_ALLOW_LIST, SUBMISSION_BY_ID);
+    public List<String> refDataCacheNames() {
+        return Arrays.asList(REF_DATA_CACHE_NAMES);
     }
 }
