05f7fd9cf204aa8fab0bb87165ddda491c6809c0
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2021-03-01 15:33:33 +0000

    Added test for formSelected log

--- src/test/java/uk/gov/companieshouse/efs/web/aspect/AspectLoggerTest.java
+++ src/test/java/uk/gov/companieshouse/efs/web/aspect/AspectLoggerTest.java
@@ -5,15 +5,20 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.CodeSignature;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.efs.web.formtemplates.model.FormTemplateModel;
 import uk.gov.companieshouse.logging.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -22,11 +27,15 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class AspectLoggerTest {
     public static final String TEST_EXCEPTION_MESSAGE = "Test Exception";
+    public static final String SUBMISSION_ID = "submissionId";
     @Mock
     Logger logger;
 
     @Mock
     JoinPoint joinPoint;
+
+    @Mock
+    FormTemplateModel mockFormTemplate;
 
     @Spy
     @InjectMocks
@@ -34,6 +43,9 @@ public class AspectLoggerTest {
 
     @Mock
     CodeSignature sig;
+
+    @Captor
+    ArgumentCaptor<Map<String, Object>> debugMapCaptor;
 
     void setUpJoinPointArgs(Map<String, Object> args) {
         when(joinPoint.getSignature()).thenReturn(sig);
@@ -50,5 +62,21 @@ public class AspectLoggerTest {
 
         verify(logger).errorContext(eq(""), eq(TEST_EXCEPTION_MESSAGE),
                 any(RuntimeException.class), any());
+    }
+
+    @Test
+    void testFormSelected() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("formTemplateAttribute", mockFormTemplate);
+        args.put("id", SUBMISSION_ID);
+        setUpJoinPointArgs(args);
+
+        testLogger.formSelected(joinPoint);
+
+        verify(logger).infoContext(eq(SUBMISSION_ID), eq("Form selected"),
+                debugMapCaptor.capture());
+
+        Map<String, Object> debugMap = debugMapCaptor.getValue();
+        assertThat(debugMap, hasKey("formSelected"));
     }
 }
