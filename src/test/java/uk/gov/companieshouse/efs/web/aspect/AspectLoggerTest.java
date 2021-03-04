ccd13f136a29e8c1c79d8a5a93ddb278d28aff70
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2021-03-04 13:27:37 +0000

    Removed unnecessary public modifier

--- src/test/java/uk/gov/companieshouse/efs/web/aspect/AspectLoggerTest.java
+++ src/test/java/uk/gov/companieshouse/efs/web/aspect/AspectLoggerTest.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class AspectLoggerTest {
+class AspectLoggerTest {
     public static final String TEST_EXCEPTION_MESSAGE = "Test Exception";
     public static final String SUBMISSION_ID = "submissionId";
     @Mock
